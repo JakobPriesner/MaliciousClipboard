df38b1f8d37bb30cac8e70c6d2d0a8d9c92ae7d2
Jakob Priesner <jakob.priesner@outlook.de>
2023-06-05 20:38:12 +0200

    init

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-# Press the green button in the gutter to run the script.
+import time
+
+import clipboard
+
 if __name__ == '__main__':
+    clipboard.copy("This is a sample Python script.")
+    print("Copied to clipboard")
+    print(clipboard.paste())
+
+    print("Please copy something")
+
+    time.sleep(10)
+
+    print(f"You have copied to clipboard: {clipboard.paste()}")
+
